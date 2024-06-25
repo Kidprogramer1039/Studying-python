9f00cfa8f378bc9c9fb9d3a8039ec4ae7ad1363b
Kid Programmer <98392401+Kidprogramer1039@users.noreply.github.com>
2024-06-25 21:07:52 +0900

    [BOJ] 벽 부수고 이동하 / 골드3 / 풀이실패
    https://www.acmicpc.net/problem/2206

---
+++ DFS/2206 벽 부수기.py
@@ -0,0 +1,20 @@
+#20:01 ~
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+
+def DFS(N,M):
+    if 
+  
+    
+    
+
+
+
+
+N,M = map(int, input().split())
+graph = []
+for i in range(N):
+    graph.append(list(input()))
+
+DFS(N,M)
